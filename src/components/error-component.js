8c80554a9553f2c95d7872f13bc072149708fe3e
jopez1 <jopez1@hotmail.com>
2023-12-28 11:37:04 -0600

    Template para error y form

---
+++ src/components/error-component.js
@@ -0,0 +1,38 @@
+import { LitElement, html, css } from 'lit';
+
+export class ErrorComponent extends LitElement {
+    static styles = [
+        css`
+            :host {
+                display: block;
+            }
+
+            p {
+                background-color: red;
+                color: black;
+                padding: 1em;
+                text-align: center;
+                border-radius: 0.25em;
+                margin-bottom:0.5em;
+                font-size: large;
+                font-weight: bold;
+            }
+        `
+    ];
+
+    static properties = {
+        message: {type: String},
+    };
+
+    constructor(){
+        super();
+        this.message = "Soy tu error preferido";
+    }
+
+    render() {
+        return html`
+        <p>${this.message}</p>
+        `;
+    }
+}
+customElements.define('error-component', ErrorComponent);
