55f30a3538b2873219409569d70ad5f276f2ad0a
jopez1 <jopez1@hotmail.com>
2023-12-28 15:21:10 -0600

    Estilos para el form

--- src/components/form-component.js
+++ src/components/form-component.js
@@ -2,42 +2,83 @@ import { LitElement, css, html } from "lit";
 import "./error-component";
 
 class FormComponent extends LitElement {
-    static styles = css`
-        :host {
-            display: block;
-        }
+  static styles = css`
+    :host {
+      display: block;
+    }
 
-        h1 {
-            color: blue;
-        }
+    h2 {
+      font-weight: bold;
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    p {
+      color: #4f46e5;
+      font-weight: bold;
+      font-size: large;
+    }
+
+    form {
+        background-color: white;
+   box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+   border-radius: 1.5rem;
+   padding-top: 2.5rem;
+   padding-bottom: 2.5rem;
+   padding-left: 1.25rem;
+   padding-right: 1.25rem;
+    }
+
+    div {
+        margin-bottom: 5em;
+    }
+
+    label {
+        color: #4a5568;
+   text-transform: uppercase;
+   font-weight: bold;
+    }
+
+    input {
+        border: 2px solid;
+   width: 100%;
+   margin-top: 0.5rem;
+   padding: 0.25rem;
+   border-radius: 0.375rem;
+    }
+    input::placeholder {
+   color: #cbd5e0;
+  `;
+
+  static properties = {
+    nombre: { type: String },
+    error: {type: Boolean}
+  };
+
+  constructor() {
+    super();
+    this.nombre = "";
+    this.error = false;
+  }
+
+  render() {
+    return html`
+      <div>
+        <h2>Seguimiento Pacientes</h2>
+
+        <p>Añade Pacientes y Administralos</p>
+
+        <form>
+            <div>
+                <label>Nombre de la Mascota</label>
+                <input id="mascota" type="text" placeholder="Nombre de Mascota"
+                />
+            </div>
+        </form>
+      </div>
     `;
-
-    static properties = {
-        form: { type: String },
-    };
-
-    constructor() {
-        super();
-        this.form = "Soy el Form";
-    }
-
-    render() {
-        return html`
-            <h1>${this.form}</h1>
-            <error-component></error-component>
-        `;
-    }
+  }
 }
 
 customElements.define("form-component", FormComponent);
 
-//Aqui utilizar conectedCallBack(){}(cuando se inicia el componente)
-/* useEffect( () => {
-    if( Object.keys(paciente).length > 0 ){
-        setNombre(paciente.nombre)
-        setPropietario(paciente.propietario)
-        setEmail(paciente.email)
-        setFecha(paciente.fecha)
-        setSintomas(paciente.sintomas)
-    }
-}, [paciente]) */
